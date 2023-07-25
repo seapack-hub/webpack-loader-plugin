660dbd7e28303b95f3fbdb5b62a35ec7af40914c
haifengzeng <3270937741@qq.com>
2023-07-25 13:23:03 +0800

    mustache模板语法 tokens与数据结合成html

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
-import {defineReactive} from "./defineReactive";
-let obj = {
-    a:5,
-    b:10
-};
-for(let key in obj){
-    defineReactive(obj,key)
-}
-console.log(obj);
+// import {defineReactive} from "./defineReactive";
+// let obj = {
+//     a:5,
+//     b:10
+// };
+// for(let key in obj){
+//     defineReactive(obj,key)
+// }
+// console.log(obj);
 
